e6c018a8af9d8566c64825d07a14cdcdff479edc
Uriel <uriel1396@gmail.com>
2024-03-13 22:01:00 -0600

    Updated traffic light project to allow two traffic ligths

--- TrafficLight/TrafficLight.ino
+++ TrafficLight/TrafficLight.ino
@@ -1,25 +1,41 @@
-const int red = 7;
-const int yellow = 6;
-const int green = 5;
+const int red1 = 7;
+const int yellow1 = 6;
+const int green1 = 5;
+
+const int red2 = 10;
+const int yellow2 = 9;
+const int green2 = 8;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(red, OUTPUT);
-  pinMode(yellow, OUTPUT);
-  pinMode(green, OUTPUT);
+  pinMode(red1, OUTPUT);
+  pinMode(yellow1, OUTPUT);
+  pinMode(green1, OUTPUT);
+
+  pinMode(red2, OUTPUT);
+  pinMode(yellow2, OUTPUT);
+  pinMode(green2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(green, HIGH);
+  digitalWrite(green1, HIGH);
+  digitalWrite(red2, HIGH);
+  digitalWrite(red1, LOW);
   delay(3000);
-  digitalWrite(green, LOW);
+  digitalWrite(green1, LOW);
   
-  digitalWrite(yellow, HIGH);
+  digitalWrite(yellow1, HIGH);
   delay(1000);
-  digitalWrite(yellow, LOW);
+  digitalWrite(yellow1, LOW);
 
-  digitalWrite(red, HIGH);
+  digitalWrite(red1, HIGH);
+  digitalWrite(red2, LOW);
+  digitalWrite(green2, HIGH);
   delay(3000);
-  digitalWrite(red, LOW);
+  digitalWrite(green2, LOW);
+
+  digitalWrite(yellow2, HIGH);
+  delay(1000);
+  digitalWrite(yellow2, LOW);
 }
