88a78cf266fa0408ab9516e3880276f118aeec2e
Uriel Olascoaga <uriel1396@gmail.com>
2025-02-04 16:06:51 -0600

    Added buzzer example

---
+++ BuzzerMusic/BuzzerMusic.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  tone(8, 400, 100);
+  delay(500);
+
+  tone(8, 800, 100);
+  delay(500);
+}
